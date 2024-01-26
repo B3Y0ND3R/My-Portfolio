6dedfb24e6d9158eabbbadcbd68742ee98ab0579
B3Y0ND3R <ahsanulhasib2@gmail.com>
2024-01-26 18:47:40 +0600

    Lab 2

---
+++ script.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const words = document.querySelectorAll(".word");
+    const animationDelay = 80;
+    const transitionDelay = 340;
+    const initialWordDelay = 1000;
+    const wordDisplayDelay = 500; 
+    const interval = 3000;
+
+    words.forEach((word) => {
+        const letters = word.textContent.split("");
+        word.textContent = "";
+        letters.forEach((letter) => {
+            const span = document.createElement("span");
+            span.textContent = letter;
+            span.className = "letter";
+            word.append(span);
+        });
+    });
+
+    let currentWordIndex = 0;
+    const maxWordIndex = words.length - 1;
+
+    const changeText = () => {
+        const currentWord = words[currentWordIndex];
+        const nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+
+        Array.from(currentWord.children).forEach((letter, i) => {
+            setTimeout(() => {
+                letter.classList.add("out");
+            }, i * animationDelay);
+        });
+
+        nextWord.style.opacity = "0";
+        Array.from(nextWord.children).forEach((letter, i) => {
+            letter.classList.remove("in", "out", "behind");
+        });
+
+        setTimeout(() => {
+            nextWord.style.opacity = "1";
+            Array.from(nextWord.children).forEach((letter, i) => {
+                setTimeout(() => {
+                    letter.classList.add("in");
+                }, (i + 1) * animationDelay);
+            });
+        }, transitionDelay + wordDisplayDelay); 
+
+        currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+    };
+
+    words[currentWordIndex].style.opacity = "1";
+
+    setTimeout(() => {
+        changeText();
+        setInterval(changeText, interval);
+    }, initialWordDelay);
+});
