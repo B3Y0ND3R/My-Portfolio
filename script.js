5a033d33a440597565a853ab105c230dc184e693
B3Y0ND3R <ahsanulhasib2@gmail.com>
2024-02-10 03:12:32 +0600

    lab 3

--- script.js
+++ script.js
@@ -1,105 +1,98 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const words = document.querySelectorAll(".word");
-    const animationDelay = 80;
-    const transitionDelay = 340;
-    const initialWordDelay = 1000;
-    const wordDisplayDelay = 1000; 
-    const interval = 3000;
-
-    words.forEach((word) => {
-        const letters = word.textContent.split("");
-        word.textContent = "";
-        letters.forEach((letter) => {
-            const span = document.createElement("span");
-            span.textContent = letter;
-            span.className = "letter";
-            word.append(span);
-        });
-    });
-
-    let currentWordIndex = 0;
-    const maxWordIndex = words.length - 1;
-
-    const changeText = () => {
-        const currentWord = words[currentWordIndex];
-        const nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
-
-        Array.from(currentWord.children).forEach((letter, i) => {
-            setTimeout(() => {
-                letter.classList.add("out");
-            }, i * animationDelay);
-        });
-
-        nextWord.style.opacity = "0";
-        Array.from(nextWord.children).forEach((letter, i) => {
-            letter.classList.remove("in", "out", "behind");
-        });
-
-        setTimeout(() => {
-            nextWord.style.opacity = "1";
-            Array.from(nextWord.children).forEach((letter, i) => {
-                setTimeout(() => {
-                    letter.classList.add("in");
-                }, (i + 1) * animationDelay);
-            });
-        }, transitionDelay + wordDisplayDelay); 
-
-        currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
-    };
-
-    words[currentWordIndex].style.opacity = "1";
-
-    setTimeout(() => {
-        changeText();
-        setInterval(changeText, interval);
-    }, initialWordDelay);
-
-
-    const scrollUpButton = document.querySelector('.scroll-to-top a');
-    const scrollDownButton = document.querySelector('.scroll-to-about a');
-
-    // Smooth scrolling for the upper arrow
-    scrollUpButton.addEventListener('click', function (e) {
-      e.preventDefault();
-
-      const targetSection = document.querySelector(this.getAttribute('href'));
-      targetSection.scrollIntoView({
-        behavior: 'smooth'
+  //Text animation
+      const words = document.querySelectorAll(".word");
+      const animationDelay = 80;
+      const transitionDelay = 340;
+      const initialWordDelay = 1000;
+      const wordDisplayDelay = 1000; 
+      const interval = 3000;
+  
+      words.forEach((word) => {
+          const letters = word.textContent.split("");
+          word.textContent = "";
+          letters.forEach((letter) => {
+              const span = document.createElement("span");
+              span.textContent = letter;
+              span.className = "letter";
+              word.append(span);
+          });
       });
-    });
-
-    // Smooth scrolling for the lower arrow
-    scrollDownButton.addEventListener('click', function (e) {
-      e.preventDefault();
-
-      const targetSection = document.querySelector(this.getAttribute('href'));
-      targetSection.scrollIntoView({
-        behavior: 'smooth'
+  
+      let currentWordIndex = 0;
+      const maxWordIndex = words.length - 1;
+  
+      const changeText = () => {
+          const currentWord = words[currentWordIndex];
+          const nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+  
+          Array.from(currentWord.children).forEach((letter, i) => {
+              setTimeout(() => {
+                  letter.classList.add("out");
+              }, i * animationDelay);
+          });
+  
+          nextWord.style.opacity = "0";
+          Array.from(nextWord.children).forEach((letter, i) => {
+              letter.classList.remove("in", "out", "behind");
+          });
+  
+          setTimeout(() => {
+              nextWord.style.opacity = "1";
+              Array.from(nextWord.children).forEach((letter, i) => {
+                  setTimeout(() => {
+                      letter.classList.add("in");
+                  }, (i + 1) * animationDelay);
+              });
+          }, transitionDelay + wordDisplayDelay); 
+  
+          currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+      };
+  
+      words[currentWordIndex].style.opacity = "1";
+  
+      setTimeout(() => {
+          changeText();
+          setInterval(changeText, interval);
+      }, initialWordDelay);
+  
+  
+      //read-more
+      const aboutContainer = document.querySelector('.about-cont');
+      const readMoreBtn = document.getElementById('read-more');
+      const readLessLink = document.getElementById('read-less');
+  
+      readMoreBtn.addEventListener('click', function (event) {
+          event.preventDefault();
+          aboutContainer.classList.add('show-full-description');
+          const aboutSection = document.getElementById('about');
+          aboutSection.scrollIntoView({
+              behavior: 'smooth'
+          });
       });
+  
+      readLessLink.addEventListener('click', function (event) {
+        event.preventDefault();
+        aboutContainer.classList.remove('show-full-description');
     });
-
-    // Show/hide the scroll buttons based on scroll position
-    window.addEventListener('scroll', function () {
-      const aboutSection = document.getElementById('about');
-      const homeSection = document.getElementById('home');
-      const scrollUpButton = document.querySelector('.scroll-to-top');
-      const scrollDownButton = document.querySelector('.scroll-to-about');
-
-      if (window.scrollY > aboutSection.offsetTop) {
-        scrollUpButton.style.display = 'block';
-      } else {
-        scrollUpButton.style.display = 'none';
-      }
-
-      if (window.scrollY < homeSection.offsetHeight) {
-        scrollDownButton.style.display = 'block';
-      } else {
-        scrollDownButton.style.display = 'none';
-      }
+  
+    var header = document.getElementById("h");
+    var navi = document.getElementById("n");
+  
+    window.addEventListener("scroll", function () {
+        var scrollPosition = window.scrollY;
+  
+        if (scrollPosition > 0) {
+            header.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--scroll-bg-color');
+            navi.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--scroll-bg-color');
+            
+        } else {
+            header.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--bg-color');
+            navi.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--bg-color');
+            
+        }
     });
-
-
-
-
   });
-
+  
+  
+  
+  
